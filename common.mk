faa9d818caae6ef6790685b67812d840813855e5
userariii <abhattacharjee717@gmail.com>
2023-06-24 17:51:02 +0000

    sdm660-common: Import speed profile services

--- common.mk
+++ common.mk
@@ -403,6 +403,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Trust
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
